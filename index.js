9541ff961991b10fa0277b91097c4d7dfac1da93
Tim <mideltim@gmail.com>
2024-07-20 23:57:21 +0800

    Add search bar to input the specific location to fetch the weather data

---
+++ index.js
@@ -0,0 +1,25 @@
+const initializeSearch = () => {
+  const form = document.getElementById("search-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const searchInput = document.getElementById("search").value;
+    getWeatherData(searchInput);
+  });
+};
+const getWeatherData = async (searchInput) => {
+  const fetchURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchInput}?unitGroup=us&key=P87L2MPWHRTGMEQBPHFEUV688&contentType=json`;
+  try {
+    const response = await fetch(fetchURL, { mode: "cors" });
+    if (response.ok) {
+      const weatherData = response.json();
+      console.log(weatherData);
+    } else {
+      console.log(`Error: ${response.status}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+document.addEventListener("DOMContentLoaded", () => {
+  initializeSearch();
+});
